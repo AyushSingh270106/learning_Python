cb3768799dca0c6c3bd1baea88b371e6c10561d2
Ayush Singh <ayush.singh.270106@gmail.com>
2025-09-05 21:08:08 +0530

    add new file

---
+++ Desktop/python/chapter/chapter1.py
@@ -0,0 +1,17 @@
+# a =int(input("Enter first no: "))
+# b = int(input("Enter second no:"))
+# print(a + b)
+
+
+# a = "Ayush Singh"
+# b = 24
+# c = 29.22
+# d = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+# Truth Table
+print()
